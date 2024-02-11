b3959c68581f30335cb8ac0f36a0f97581c6c3da
mzayni <127202966+mzayni@users.noreply.github.com>
2024-02-11 13:31:53 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -25,3 +25,11 @@ func Max(x, y float64) float64 {
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
 }
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(x, y)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
